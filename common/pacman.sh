ef7dafa47f7022a81b348c97961adfce95c859ef
Marcin Rogacki <rogacki.m@gmail.com>
2016-12-06 15:13:57 +0100

    bash: lsyncnotify
    new file:   common/bashrc/.slash/usr/bin/lsyncdnotify
    modified:   common/pacman.sh

--- common/pacman.sh
+++ common/pacman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo -e "\e[32mINSTALL STOW SOFTWARES FOR: COMMON\e[0m"
-pacman -S xbindkeys xdotool synergy xscreensaver
+pacman -S xbindkeys xdotool synergy xscreensaver notification-daemon libnotify
 
 if [ -f ./pacman-env.sh ]; then
     echo -e "\e[32mINSTALL STOW SOFTWARES FOR: ENV\e[0m"
