120d6454ae0a4af6dbcd3a60773efbf2f8b25333
Marcin Rogacki <rogacki.m@gmail.com>
2016-12-23 08:16:00 +0100

    games: Tibia mode in capslock, dunst - notification server
    Caps lock now is completly disabled. Has been mapped to Void Symbol
    by setxkbmap.
    
    modified:   common/fluxbox/.fluxbox/init
    modified:   common/fluxbox/.fluxbox/keys
    modified:   common/games/.slash/usr/bin/xbindkeystibiatoggle.sh
    modified:   common/games/.xbindkeysrc
    modified:   common/games/.xbindkeysrc-tibia
    modified:   common/pacman.sh
    modified:   robaknb/X11/.xinitrc
    modified:   robaknb/fluxbox/.fluxbox/startup
    modified:   robakpc/X11/.xinitrc
    deleted:    robakpc/fluxbox
    new file:   robakpc/fluxbox/.fluxbox/apps
    new file:   robakpc/fluxbox/.fluxbox/init
    new file:   robakpc/fluxbox/.fluxbox/keys
    new file:   robakpc/fluxbox/.fluxbox/startup
    new file:   robakpc/fluxbox/.fluxbox/styles

--- common/games/.slash/usr/bin/xbindkeystibiatoggle.sh
+++ common/games/.slash/usr/bin/xbindkeystibiatoggle.sh
@@ -10,9 +10,11 @@ if [ -n "$result" ]; then
     xbindkeys -f ~/.xbindkeysrc 2>/dev/null
     # set previus fluxbox style
     xdotool key Alt+Shift+F1
+    notify-send  "Tibia mode on" "CAPSLOCK to diable. Some keyboard keys like 'wasd' are unavailable!" --icon=dialog-information
 else
     killall -9 xbindkeys 2>/dev/null
     xbindkeys -f ~/.xbindkeysrc-tibia 2>/dev/null
     # set fluxbox style which will inform that keys are redefined
     xdotool key Alt+Shift+F2
+    notify-send  "Tibia mode off" "CAPSLOCK to enable. Keyboard keys back to normal" --icon=dialog-information
 fi
