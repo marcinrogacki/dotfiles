53ad089c5338754fe45e8cb93aef96654169d214
Marcin Rogacki <mrogacki@nexway.com>
2017-04-07 19:56:32 +0200

    vim: install ctags using pacman
    modified:   common/pacman.sh

--- common/pacman.sh
+++ common/pacman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo -e "\e[32mINSTALL STOW SOFTWARES FOR: COMMON\e[0m"
-pacman -S synergy xscreensaver libnotify dunst the_silver_searcher
+pacman -S synergy xscreensaver libnotify dunst the_silver_searcher ctags
 
 if [ -f ./pacman-env.sh ]; then
     echo -e "\e[32mINSTALL STOW SOFTWARES FOR: ENV\e[0m"
