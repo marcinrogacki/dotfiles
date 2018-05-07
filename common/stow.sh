816e2962593cddbea184376ef8a1a0ca2d9a5a27
Marcin Rogacki <mrogacki@nexway.com>
2018-05-07 12:31:41 +0200

    common: automatization of stow.sh
    modified:   common/stow.sh

--- common/stow.sh
+++ common/stow.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-git submodule update --init
+env=${PWD##*/}
+case "$env" in
+    *@*)
+        ## All correct, we can procceed.
+        ;;
+    *)
+        >&2 echo "$env - not a env! Are you in stow directory? Must be properly named with domain separator <user>@<domain>."
+        exit 1
+        ;;
+esac
 
-stow -t ~ bashrc
-stow -t ~ fluxbox
-stow -t ~ git
-stow -t ~ vim
-stow -t ~ ctags
-stow -t ~ tmux
-stow -t ~ X11
-stow -t ~ games
-stow -t ~ dunst
+for dir in */; do
+    case "$@" in
+        *--revert*)
+            stow -Dt ~ $dir
+            ;;
+        *)
+            stow -t ~ $dir
+            ;;
+    esac
+done
