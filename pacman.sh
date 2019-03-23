a51f0c413d91ee42f3cfa2c935ea06f7828373de
Marcin Rogacki <mrogacki@nexway.com>
2019-03-23 20:37:05 +0100

    base: minor fixes

--- pacman.sh
+++ pacman.sh
@@ -43,4 +43,16 @@ if [ ! -d "$env_dir" ]; then
     exit 1
 fi
 
-pacman -S --needed $(find -L . -name pacman.dep | xargs cat | grep -v '^#' | sort -u)
+# find -L "$env_dir" -name pacman.dep | xargs cat | grep -v '^#' | sort -u
+# pacman -S --needed $(find -L "$env_dir" -name pacman.dep | xargs cat | grep -v '^#' | sort -u)
+
+# for dep in `find -L "$env_dir" -name pacman.dep | xargs cat | grep -v '^#' | sort -u`; do
+#     pacman -Rs "$dep"
+# done
+
+for dep_file in `find -L "$env_dir" -name pacman.dep`; do
+                                  echo -e "\e[32m[FILE]\e[39m $dep_file"
+    cat "$dep_file" | xargs -I {} echo -e "\e[32m[SOFT]\e[39m "{}
+    pacman -S --needed $(cat "$dep_file" | grep -v '^#' | sort -u)
+    # pacman -Rs $(cat "$dep_file" | xargs cat | grep -v '^#' | sort -u)
+done
