3e88a3a485d886978b839826efd18c48ccf0cee4
Marcin Rogacki <mrogacki@nexway.com>
2020-04-29 11:35:53 +0200

    vim: Add branch name hook skips merge messages

--- repository/git/.gitglobal/hooks/prepare-commit-msg/git/add-branch-name-to-commit-message.sh
+++ repository/git/.gitglobal/hooks/prepare-commit-msg/git/add-branch-name-to-commit-message.sh
@@ -3,10 +3,8 @@ COMMIT_MSG_FILE=$1
 COMMIT_SOURCE=$2
 SHA1=$3
 
-# --amend case
-if [ "$SHA1" == "HEAD" ]; then
-    exit 0
+# When empty we are in simple commit case (no --amend nor non fast-forward merge)
+if [[ -z "$SHA1" && -z "$COMMIT_SOURCE" ]]; then
+    # Add branch name at the beginning of the commit message
+    echo -e "`git rev-parse --abbrev-ref HEAD`\n$(cat "$COMMIT_MSG_FILE")" > $COMMIT_MSG_FILE
 fi
-
-# Add branch name at the beginning of the commit message
-echo -e "`git rev-parse --abbrev-ref HEAD`\n$(cat "$COMMIT_MSG_FILE")" > $COMMIT_MSG_FILE
