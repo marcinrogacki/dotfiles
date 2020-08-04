98ab6babc6c11f180902644485323a4366d0ad04
Marcin Rogacki <mrogacki@nexway.com>
2020-08-04 21:42:11 +0200

    bashrc: fluxbo: ioproc as bin, change theme

--- repository/bashrc/.slash/etc/bashrc.d/500-bash-aliases.sh
+++ repository/bashrc/.slash/etc/bashrc.d/500-bash-aliases.sh
@@ -1,9 +1,1 @@
-# Allows check a program i/o by searching a string in process list (e.g. by parameter)
-function ioproc {
-    program_name="$1"
-    echo "writes (MB): $1"
-    ps aux | grep "$program_name" | awk '{print $2}' | xargs -I {} cat /proc/{}/io 2>/dev/null | grep '^write_bytes' | awk '{print $2/1024/1024}'
-    echo
-    echo "reads (MB): $1"
-    ps aux | grep "$program_name" | awk '{print $2}' | xargs -I {} cat /proc/{}/io 2>/dev/null | grep '^read_bytes' | awk '{print $2/1024/1024}'
-}
+
