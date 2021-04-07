3f72778dba509fa3f0e1bbc736d148b696e932c9
Marcin Rogacki <mrogacki@nexway.com>
2021-04-07 13:44:49 +0200

    fzf: Init and add to all environments

---
+++ repository/fzf/.slash/etc/bashrc.d/100-fzf.sh
@@ -0,0 +1,4 @@
+# Those files are provided by the Arch Linux after a installation of the fzf
+# package.
+source /usr/share/fzf/key-bindings.bash
+source /usr/share/fzf/completion.bash
---
+++ repository/fzf/README.md
@@ -0,0 +1,4 @@
+# About
+
+Command line file/directory finding booster.
+https://www.freecodecamp.org/news/fzf-a-command-line-fuzzy-finder-missing-demo-a7de312403ff/
