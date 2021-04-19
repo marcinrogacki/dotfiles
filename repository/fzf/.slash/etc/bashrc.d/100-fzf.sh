2db84d059c75d113698dfc656ee107e9fe00889d
Marcin Rogacki <mrogacki@nexway.com>
2021-04-19 12:27:34 +0200

    fzf: alt-c respect .gitignore and search hidden

--- repository/fzf/.slash/etc/bashrc.d/100-fzf.sh
+++ repository/fzf/.slash/etc/bashrc.d/100-fzf.sh
@@ -2,3 +2,6 @@
 # package.
 source /usr/share/fzf/key-bindings.bash
 source /usr/share/fzf/completion.bash
+
+# Exclude files from .gitignore and search hidden
+export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
