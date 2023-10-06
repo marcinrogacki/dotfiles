856507a7bae88879ff0c52aafd0fd878d95b007c
Marcin Rogacki <mrogacki@nexway.com>
2023-10-06 12:44:39 +0200

    repository.sh: Change arg order and fix dependency installation

--- repository.sh
+++ repository.sh
@@ -2,49 +2,31 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+# Remove trailing slash from arguments
+repo_path="${1%/}"
+env_path="${2%/}"
+
 display_usage() {
 cat << USAGE
-Usage: sh `basename $0` ENVIRONMENT DOTFILES
+Usage: sh `basename $0` WHAT WHERE
 
 Link dotfiles from repository into given environment.
 Main purpose of this script is to manages dependencies. Recursively links other
 dotfiles which must be added into environment.
 
-DOTFILES
-    One from repository directory.
+WHAT
+    One of subdirectory (config set) from repository directory.
 
-ENVIRONMENT
-    One from environment directory.
+WHERE
+    One from subdirectory from environment directory.
 
 COMMAND
     -h|--help   Display this message.
 
 Example
-    sh `basename $0` environment/mrogacki@homepc repository/bashrc
+    sh `basename $0` repository/bashrc environment/mrogacki@homepc
     sh `basename $0` -h
 USAGE
-}
-
-link_repository() {
-    env_dir="$1"
-    repository="$2"
-    cd "$env_dir";
-    # easier than check if already exists with same link
-    dotfile=`basename "$repository"`
-    unlink "$dotfile" 2>/dev/null
-    ln -s ../../"$repository"
-
-    dependency_file=../../"$repository"/dotfiles.dep
-    if [ ! -f "$dependency_file" ]; then
-        exit 0;
-    fi
-
-    for dependency_dotfile in `cat "$dependency_file"`; do
-        if [ ! -d "$dependency_dotfile" ]; then
-            echo "installing dependency: $dotfile needs $dependency_dotfile"
-            link_repository "$env_dir" "repository/$dependency_dotfile"
-        fi
-    done
 }
 
 for arg in "$@"; do
@@ -54,28 +36,52 @@ for arg in "$@"; do
     fi
 done
 
-env="${1%/}"
-if [[ "$env" != environment/* ]]; then
-    >&2 echo "Wrong environment provided. Please provide one from 'environment' directory."
+repo_full_path="$SCRIPT_DIR/$repo_path"
+if [ ! -d "$repo_full_path" ]; then
+    >&2 echo "Not existing repository. Directory does not exists $repo_full_path"
     exit 1
 fi
 
-env_dir="$SCRIPT_DIR/$env"
-if [ ! -d "$env_dir" ]; then
-    >&2 echo "Not existing environment. Directory does not exists $env_dir"
+env_full_path="$SCRIPT_DIR/$env"
+if [ ! -d "$env_full_path" ]; then
+    >&2 echo "Not existing environment. Directory does not exists $env_full_path"
     exit 1
 fi
 
-repository="${2%/}"
-if [[ "$repository" != repository/* ]]; then
-    >&2 echo "Wrong repository provided. Please provide one from 'repository' directory."
-    exit 1
-fi
+link_repository() {
+    arg_repo_path="$1"
+    arg_env_path="$2"
+    arg_prefix="$3"
+    arg_repo_full_path="$SCRIPT_DIR/$arg_repo_path"
+    arg_env_full_path="$SCRIPT_DIR/$arg_env_path"
+    # easier than check if already exists with same link
+    unlink "$arg_env_full_path/`basename $arg_repo_path`" 2>/dev/null
+    echo "${arg_prefix}Install `basename $arg_repo_path` in `basename $arg_env_path`"
+    ln -s ../../"$arg_repo_path" $arg_env_path
 
-repository_dir="$SCRIPT_DIR/$repository"
-if [ ! -d "$repository_dir" ]; then
-    >&2 echo "Not existing repository. Directory does not exists $repository_dir"
-    exit 1
-fi
 
-link_repository "$env_dir" "$repository"
+    # cd "$env_dir";
+    # easier than check if already exists with same link
+    # dotfile=`basename "$repository"`
+    # unlink "$dotfile" 2>/dev/null
+    # echo "Install $dotfile in $env_dir"
+    # ln -s ../../"$repository"
+
+    dependency_repo_path=$arg_repo_path/dotfiles.dep
+    dependency_repo_full_path="$SCRIPT_DIR/$dependency_repo_path"
+    if [ ! -f "$dependency_repo_full_path" ]; then
+        exit 0;
+    fi
+
+    for dependency_repo_name in `cat "$dependency_repo_full_path"`; do
+        dependency_repo_path="repository/$dependency_repo_name"
+        dependency_repo_full_path="$SCRIPT_DIR/$dependency_repo_path"
+        if [ -d "$dependency_repo_full_path" ]; then
+            link_repository "$dependency_repo_path" "$arg_env_path" "$arg_prefix  "
+        else
+            >&2 echo "Not existing dependency repository. Directory does not exists: $dependency_repo_full_path. Dependency file: $dependency_repo_path"
+        fi
+    done
+}
+
+link_repository "$repo_path" "$env_path"
