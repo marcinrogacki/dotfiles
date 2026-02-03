fdccdfd19fe369b009bb565670fabac7ae742206
Marcin Rogacki <mrogacki@nexway.com>
2026-02-03 16:03:54 +0100

    games: poemapsclicker.sh Faustus support

--- repository/games/.slash/bin/poemapsclicker.sh
+++ repository/games/.slash/bin/poemapsclicker.sh
@@ -8,28 +8,29 @@ focused_window_id=$(xdotool getwindowfocus)
 # Get the ID of the application's window
 app_window_id=$(xdotool search --name "$app_name" | head -1)
 
-if [ "$focused_window_id" != "$app_window_id" ]; then
-    echo 'Not focused' $app_name
-    exit
-fi
+# if [ "$focused_window_id" != "$app_window_id" ]; then
+#     echo 'Not focused' $app_name
+#     exit
+# fi
 
 mouse_location=$(xdotool getmouselocation --shell)
 eval $mouse_location
 
 # Steps for clicking
-step=50
+w_step=50
+h_step=48
 
-if [ $1 == 2 ]; then
-    width=$((step*6))
-    height=$((step*6))
-else
-    width=$((step*12))
-    height=$((step*6))
-fi
+# if [ $1 == 2 ]; then
+#    width=$((w_step*6))
+#    height=$((h_step*6))
+# else
+    width=$((w_step*12))
+    height=$((h_step*6))
+# fi
 
 # Calculate the number of steps in both x and y directions
-steps_x=$((width / step))
-steps_y=$((height / step))
+steps_x=$((width / w_step))
+steps_y=$((height / h_step))
 
 start_X=$X
 start_Y=$Y
@@ -42,8 +43,8 @@ for ((j = 0; j < steps_x; j++)); do
     # Loop through each row
     for ((i = 0; i < steps_y; i++)); do
         # Calculate current position
-        current_x=$((start_X + j * step))
-        current_y=$((start_Y + i * step))
+        current_x=$((start_X + j * w_step))
+        current_y=$((start_Y + i * h_step))
 
         # Move to the current position
         xdotool mousemove $current_x $current_y
@@ -59,6 +60,13 @@ for ((j = 0; j < steps_x; j++)); do
         xdotool keydown Control
         xdotool click 1
         xdotool keyup Control
+        sleep 0.1
+        # Confir Enter to Faustus shop (F9)
+        if [ $1 == 2 ]; then
+            xdotool keydown Enter
+            xdotool keyup Enter
+        fi
+
     done
 done
 
