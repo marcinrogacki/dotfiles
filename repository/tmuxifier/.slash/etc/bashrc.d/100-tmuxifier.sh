bda1690650f618bdadb9642b6cd0b55156889940
Marcin Rogacki <mrogacki@nexway.com>
2026-04-13 14:08:13 +0200

    tmuxifier: delete from dotfiles

--- repository/tmuxifier/.slash/etc/bashrc.d/100-tmuxifier.sh
+++
@@ -1,3 +0,0 @@
-export PATH="$PATH:$HOME/.slash/lib/tmuxifier/bin"
-export TMUXIFIER_LAYOUT_PATH="$HOME/.slash/etc/tmuxifier/"
-eval "$(tmuxifier init -)"
--- repository/tmuxifier/.slash/etc/tmuxifier/llm.window.sh
+++
@@ -1,2 +0,0 @@
-new_window "llm"
-run_cmd "cursor-agent"
